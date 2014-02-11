154015dafe039138acad16e605b5691ba5b2d183
Edgar <aedgar1@engr-131-12.eastern.ewu.edu>
2014-02-11 14:53:12 -0800

    1
    added transit.js, look at animate.css

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).foundation();
+
+$(".box").transition({
+
+	x:'399px',
+	y:'200px',
+	perspective: '100px',
+  rotateY: '180deg'
+
+}, 10000)
+.transition({
+	x:'200'
+
+})
+
+;
+
+console.log("OK USA!");
